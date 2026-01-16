56dbd0d6590faee2f8f8663d13ba6e1f07c329a8
anhlieem <leminhhuy06102005@gmail.com>
2026-01-16 09:36:23 +0700

    dl

--- layout/error404.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!defined('APP_BOOTSTRAPPED')) { http_response_code(404); exit; }
-http_response_code(404);
-?>
-<section class="container py-5 text-center">
-  <h1 class="display-6">404 - Không tìm thấy trang</h1>
-  <p class="text-muted">Trang bạn yêu cầu hiện không tồn tại hoặc đã được di chuyển.</p>
-  <a class="btn btn-success" href="index.php">Về trang chủ</a>
-</section>
