547c6d80b7b98154998cb2d8caa1b8d7bd4d5a97
VThangws <trinhvietthang27@gmail.com>
2026-03-17 21:16:11 +0700

    giao dien admin

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin - Hệ thống quản lý thư viện</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="icon" type="image/png" href="/assets/img/logo-library/library.png">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,300,0,200" />
+
+    <link rel="stylesheet" href="/assets/css/header.css">
+    <link rel="stylesheet" href="/assets/css/slide.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
+    <link rel="stylesheet" href="/assets/fonts/font.css">
+    <link rel="stylesheet" href="/assets/css/books.css">
+</head>
+<body>
+<?php
+include __DIR__ . '/layout/admin_header.php';
+include __DIR__ . '/layout/admin_dashboard.php';
+include __DIR__ . '/../layout/footer.php';
+?>
+
+<script src="/assets/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+</body>
+</html>
